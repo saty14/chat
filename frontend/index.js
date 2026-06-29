1736e2f162d685208b498b115623424ef57cbf21
saty14 <satyam.tiwari@parkhya.net>
2026-06-29 14:58:10 +0530

    initial commit

---
+++ frontend/index.js
@@ -0,0 +1,75 @@
+
+const currentScript = document.currentScript;
+const oauthId = currentScript.dataset.oauthId;
+const oauthDomen = currentScript.dataset.oauthDomen;
+
+console.log(oauthId); // 123456
+
+if (oauthId !== "12345") {
+  throw new Error("Invalid OAuth ID");
+}
+
+if (oauthDomen !== "mysite.com") {
+  throw new Error("Invalid OAuth Domen");
+}
+
+
+
+(function () {
+
+    const button = document.createElement("div");
+
+    button.innerHTML = "💬";
+
+    Object.assign(button.style, {
+        position: "fixed",
+        right: "20px",
+        bottom: "20px",
+        width: "60px",
+        height: "60px",
+        borderRadius: "50%",
+        background: "#2563eb",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "28px",
+        cursor: "pointer",
+        zIndex: "999999",
+        boxShadow: "0 5px 15px rgba(0,0,0,.3)"
+    });
+
+    const chat = document.createElement("div");
+
+    chat.style.cssText = `
+        position:fixed;
+        right:20px;
+        bottom:90px;
+        width:350px;
+        height:600px;
+        background:white;
+        border-radius:10px;
+        box-shadow:0 5px 20px rgba(0,0,0,.3);
+        display:none;
+        overflow:hidden;
+        z-index:999999;
+    `;
+
+    chat.innerHTML = `
+        <iframe
+            src="http://127.0.0.1:5500/frontend/chat.html"
+            style="width:100%;height:100%;border:none;">
+        </iframe>
+    `;
+
+    button.onclick = () => {
+        chat.style.display =
+            chat.style.display === "none" ? "block" : "none";
+    };
+
+    document.body.appendChild(button);
+    document.body.appendChild(chat);
+
+})();
+
+
